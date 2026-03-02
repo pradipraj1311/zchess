821630d87aa9943e4320ff27bf04c0460b5e480e
Pradip Rajwadi <24ceuts125@ddu.ac.in>
2026-03-02 18:29:46 +0000

    Reinitialized project with layered architecture and Spring Security setup

--- src/main/java/com/zchess/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zchess.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.zchess.entity.Game;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
